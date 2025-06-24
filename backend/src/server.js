6bf0bdb1304d629b8a13728f5f5077cde0bb6482
Dalip Kumar <dalipyadav68@gmail.com>
2025-06-24 17:40:17 +0530

    MongoDB Added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 import notesRoutes from './routes/notesRoutes.js';
 
+dotenv.config();
 const app = express();
 
+app.use(express.json());
+connectDB();
+
 app.use('/api/notes', notesRoutes);
+
 app.listen(5001, () => {
   console.log('Server started on PORT:5001');
 });
